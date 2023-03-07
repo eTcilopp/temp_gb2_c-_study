cde02ec6350519d8ae7bcf1e236c5bb07bbff305
Alex Kirikeza <eTcilopp@users.noreply.github.com>
2023-03-07 03:09:48 +0000

    ToLower()

---
+++ Examples/Example005_Condition_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter username ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "al")
+{
+    Console.WriteLine("Hello,  Al!");
+}
+else
+{
+    Console.WriteLine("Hello, man!");
+}
