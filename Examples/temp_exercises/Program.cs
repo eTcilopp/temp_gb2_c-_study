224cadb8dfe6a9d5123602fe2719c476e91a32e7
Alex Kirikeza <eTcilopp@users.noreply.github.com>
2023-03-09 15:07:03 +0000

    doing some exercises

---
+++ Examples/temp_exercises/Program.cs
@@ -0,0 +1,14 @@
+﻿void NextFib(int limit, int prev1 = 0, int prev2 = 1)
+{
+    int new_val = 0;
+    int counter = 0;
+    while (counter <= limit)
+    {
+        new_val = prev1 + prev2;
+        Console.WriteLine(new_val);
+        NextFib(limit, prev2, new_val);
+        counter++;
+    }
+}
+
+NextFib(10);
