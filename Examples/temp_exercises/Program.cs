a8020d908b791da082f7d12986ecf080bad4a228
Alex Kirikeza <eTcilopp@users.noreply.github.com>
2023-03-13 00:01:52 +0000

    running dog problem

--- Examples/temp_exercises/Program.cs
+++ Examples/temp_exercises/Program.cs
@@ -1,14 +1,65 @@
-﻿void NextFib(int limit, int prev1 = 0, int prev2 = 1)
+﻿// // Задача с собакой
+
+float person1Position = 0;
+float person2Position = 100;
+float dogPosition = 0;
+
+
+MovingObject p1 = new MovingObject("Pers 1", person1Position, 5, 1);
+MovingObject p2 = new MovingObject("Pers 2", person2Position, 7, -1);
+MovingObject dog = new MovingObject("Dog", dogPosition, 10, 1);
+
+float t = 0;
+int dogChangeDirectionCount = 0;
+
+while (person1Position < person2Position)
 {
-    int new_val = 0;
-    int counter = 0;
-    while (counter <= limit)
-    {
-        new_val = prev1 + prev2;
-        Console.WriteLine(new_val);
-        NextFib(limit, prev2, new_val);
-        counter++;
-    }
+    person1Position = p1.GetPosition(t);
+    person2Position = p2.GetPosition(t);
+    dogPosition = dog.GetPosition(t);
+    if ((dog.GetDirection() == 1 && dogPosition >= person2Position ) | (dog.GetDirection() == -1 && dogPosition <= person1Position))
+        {
+            dog.ChangeDirection();
+            dogChangeDirectionCount++;
+        }
+    Console.WriteLine($"{person1Position}-{dogPosition}-{person2Position}");
+    t += 1;
 }
 
-NextFib(10);
+Console.WriteLine($"Dog changed direction {dogChangeDirectionCount} times.");
+
+class MovingObject
+{
+    string objectName;
+    float previousPosition;
+    float previousTime;
+    int speed;
+    int direction;
+
+    public MovingObject(string name, float previousPosition, int speed, int direction)
+    {
+        this.objectName = name;
+        this.previousPosition = previousPosition;
+        this.speed = speed;
+        this.direction = direction;
+        this.previousTime = 0;
+    }
+    public int GetDirection()
+        {
+            return this.direction;
+        }
+
+    public float GetPosition(float currentTime)
+    {
+        float currentPosition = this.previousPosition + (currentTime-this.previousTime) * this.speed * this.direction;
+        this.previousPosition = currentPosition;
+        this.previousTime = currentTime;
+        return currentPosition;
+    }
+
+    public void ChangeDirection()
+        {
+            this.direction = -this.direction;
+        }
+
+}
