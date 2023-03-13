46d8d2ca3cb1c58d00edab3b6a49914d3f11f10a
Alex Kirikeza <eTcilopp@users.noreply.github.com>
2023-03-13 00:11:11 +0000

    fixed float error

--- Examples/temp_exercises/Program.cs
+++ Examples/temp_exercises/Program.cs
@@ -1,15 +1,16 @@
 ﻿// // Задача с собакой
 
-float person1Position = 0;
-float person2Position = 100;
-float dogPosition = 0;
+float person1Position = 0F;
+float person2Position = 100F;
+float dogPosition = 0F;
+string directionSign;
 
 
 MovingObject p1 = new MovingObject("Pers 1", person1Position, 5, 1);
 MovingObject p2 = new MovingObject("Pers 2", person2Position, 7, -1);
 MovingObject dog = new MovingObject("Dog", dogPosition, 10, 1);
 
-float t = 0;
+float t = 0F;
 int dogChangeDirectionCount = 0;
 
 while (person1Position < person2Position)
@@ -17,13 +18,22 @@ while (person1Position < person2Position)
     person1Position = p1.GetPosition(t);
     person2Position = p2.GetPosition(t);
     dogPosition = dog.GetPosition(t);
+    if (dog.GetDirection() == 1)
+    {
+        directionSign = "->";
+    }
+    else
+    {
+        directionSign = "<-";
+    }
     if ((dog.GetDirection() == 1 && dogPosition >= person2Position ) | (dog.GetDirection() == -1 && dogPosition <= person1Position))
         {
             dog.ChangeDirection();
             dogChangeDirectionCount++;
         }
-    Console.WriteLine($"{person1Position}-{dogPosition}-{person2Position}");
-    t += 1;
+    // Console.WriteLine($"{person1Position}:{dogPosition} {directionSign}:{person2Position}");
+    // Console.WriteLine(t);
+    t += 0.1F;
 }
 
 Console.WriteLine($"Dog changed direction {dogChangeDirectionCount} times.");
@@ -42,7 +52,7 @@ class MovingObject
         this.previousPosition = previousPosition;
         this.speed = speed;
         this.direction = direction;
-        this.previousTime = 0;
+        this.previousTime = 0F;
     }
     public int GetDirection()
         {
