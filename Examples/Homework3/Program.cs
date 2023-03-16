faacc81d5c5b0b9cfc7e8e92a9b9ba57f9a7b766
Alex Kirikeza <eTcilopp@users.noreply.github.com>
2023-03-16 03:25:09 +0000

    homework 3

--- Examples/Homework3/Program.cs
+++ Examples/Homework3/Program.cs
@@ -8,32 +8,63 @@ Array.Reverse(charArray);
 
 if (new string(charArray) == five_digit_num)
 {
-    System.Console.WriteLine($"palindrome");
+    System.Console.WriteLine($"Palindrome");
 }
 else
 {
-    System.Console.WriteLine("not palindrome");
+    System.Console.WriteLine("Not a palindrome");
 }
+
+// Задача 19. Вариант 2
+
+decimal five_digit_num_num = Convert.ToDecimal(five_digit_num);
+
+int lastNumericalDigit = 1;
+while (five_digit_num_num/lastNumericalDigit >= 1)
+{
+    lastNumericalDigit *= 10; 
+}
+
+int firstNumericalDigit = 1;
+string res = "";
+while (lastNumericalDigit >= 10)
+{
+    decimal nextDigit = Math.Floor(((decimal)five_digit_num_num % lastNumericalDigit) / (lastNumericalDigit /10)); //num%100//10
+    decimal digitToCompare = Math.Floor((decimal)five_digit_num_num % (firstNumericalDigit * 10) / firstNumericalDigit); //num%100//10
+    if (nextDigit != digitToCompare)
+    {
+        res = " not";
+        break;
+    }
+    lastNumericalDigit /= 10;
+    firstNumericalDigit *= 10;
+}
+System.Console.WriteLine($"This is{res} a palindrome");
+
+
 
 //Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-// int x1 = int.Parse(Console.ReadLine())!;
-int x1 = Convert.ToInt32(Console.ReadLine());
-int y1 = Convert.ToInt32(Console.ReadLine());
-int z1 = Convert.ToInt32(Console.ReadLine());
-int x2 = Convert.ToInt32(Console.ReadLine());
-int y2 = Convert.ToInt32(Console.ReadLine());
-int z2 = Convert.ToInt32(Console.ReadLine());
 
+char[] arr = {'x', 'y', 'z'};
+double sum = 0;
+foreach (char axe in arr)
+{
+    double[] pos = new Double[3]; 
+    for (int i=1; i <=2; i++)
+    {
+        System.Console.Write($"Enter coordinate {axe}{i}: ");
+        pos[i] = Convert.ToInt32(Console.ReadLine())!;
+    }
+    sum += Math.Pow((pos[2] - pos[1]), 2);
+}
 
-double distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)  + Math.Pow((z2 - z1), 2));
+System.Console.WriteLine($"Distance = {Math.Sqrt(sum)}");
 
-
-System.Console.WriteLine(distance);
 
 // Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-System.Console.Write("Enter N :");
+System.Console.Write("Enter N: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 for (int n=1; n <= num; n++)
