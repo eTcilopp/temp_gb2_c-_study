86e7a13f2e9f2e0f3b765f10c8c5c9094a20b18b
Alex Kirikeza <eTcilopp@users.noreply.github.com>
2023-03-19 02:01:37 +0000

    hw4 is ready

--- Examples/Homework4/Program.cs
+++ Examples/Homework4/Program.cs
@@ -13,6 +13,7 @@ int power(int num_a, int num_b)
 }
 
 int res = power(3, 7); //2187
+System.Console.WriteLine("Problem 25, var. 1");
 System.Console.WriteLine(res);
 
 // Вариант 2
@@ -27,4 +28,43 @@ int power_(int num1, int num2)
     return res2;
 }
 int res2 = power_(3, 7); //2187
+System.Console.WriteLine("Problem 25, var. 2");
 System.Console.WriteLine(res2);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int getNumberSum(int num)
+{
+    int sum = 0;
+    while(num > 0)
+    {
+        sum += num%10;
+        num /= 10;
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Problem 27");
+System.Console.WriteLine(getNumberSum(9012)); //12
+System.Console.WriteLine(getNumberSum(452)); //11
+System.Console.WriteLine(getNumberSum(82)); //11
+
+// Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)
+
+int[] getArray(int arraySize)
+    {
+        int[] resArray = new int[arraySize];
+        Random random = new Random();
+        for (int i=0; i<arraySize; i++)
+        {
+            resArray[i] = random.Next(0, 100);
+        }
+        return resArray;
+    }
+
+System.Console.WriteLine("Problem 29");
+foreach (int number in getArray(10))
+{
+    System.Console.WriteLine($"Array el: {number}");
+}
+
