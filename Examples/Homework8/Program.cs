7226ff3c1b83bb36bf189bae194c3691cfd58248
Alex Kirikeza <eTcilopp@users.noreply.github.com>
2023-04-01 03:00:12 +0000

    problem 3 - working

--- Examples/Homework8/Program.cs
+++ Examples/Homework8/Program.cs
@@ -97,3 +97,33 @@ int[,] myArray2 = new int [,]
 
 Print2dArray(myArray2);
 PrintArrayFA(myArray2);
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] matrix1 = new int [,]
+{
+    {2, 4},
+    {3, 2}
+};
+
+int[,] matrix2 = new int [,]
+{
+    {3, 4},
+    {3, 3}
+};
+
+void MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+{   
+    // int [,] resMatrix = new int[matrix1.GetLength(0),matrix2.GetLength(1)]
+    for (int i=0; i<matrix1.GetLength(0); i++)
+    {   
+        int fu = 0;
+        for (int j=0; j<matrix2.GetLength(1); j++)
+        {
+            fu += matrix1[i, j] * matrix1[j, i];
+        }
+        System.Console.WriteLine(fu);
+    }
+}
+
+MultiplyMatrix(matrix1, matrix2);
